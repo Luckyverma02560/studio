bdbe2d7140300ca6c9672e7bba52b7ff4fcdb6ce
Lucky Verma. L.V <kavirajverma1976@gmail.com>
2025-11-23 09:14:36 +0000

    email on the another line downwards too (_for element p_)

--- src/components/disclaimer-section.tsx
+++ src/components/disclaimer-section.tsx
@@ -17,7 +17,7 @@ export const DisclaimerSection = () => {
 You must be at least 18 years old to participate in this Stock Market Trading Workshop.
 
 For Educational Purposes Only:
-The information presented in this Stock Market Trading Workshop is intended for educational purposes only and does not constitute financial advice or a solicitation to buy or sell any financial instruments. The information is provided “as is” without warranty of any kind, express or implied, including but not limited to the implied warranties of merchantability, fitness for a particular purpose, or non-infringement.
+The information presented in this Stock Market Trading Workshop is intended for educational purposes only and does not constitute financial advice or a solicitation to buy or sell any financial instruments. The information is provided “as is” without warranty of any kind, express or implied, including but not to the implied warranties of merchantability, fitness for a particular purpose, or non-infringement.
 
 Investment Risk:
 Investing in the stock market involves inherent risks, and there is no guarantee of profit. Participants in this workshop should be aware of the risks involved and should consult with a licensed financial professional before making any investment decisions. Past performance is not indicative of future results.
@@ -43,7 +43,8 @@ You agree to use the information provided in this workshop responsibly and ethic
                     <AnimateOnScroll animationClasses="animate-fade-in-up" className="animation-delay-400">
                         <p className="whitespace-pre-line text-red-500 text-lg font-helvetica">
 {`All The Rights Of This Prototypes Are Reserved  And the information and images shown here is a prototype and Non Copyrighted. Remember, this Website Is For Sale.                                                                                         For More Details,                                             
-Contact : +91 8600070638                                        Email : kavirajverma1976@gmail.com`}
+Contact : +91 8600070638                                        
+Email : kavirajverma1976@gmail.com`}
                         </p>
                     </AnimateOnScroll>
                 </div>
